c8808d9ea6d7a572e6b87bdb189277f44b0f52fc
Thomas Semiz <tas56@njit.edu>
2021-04-24 15:08:13 -0400

    Created refresh tokens

--- src/auth.js
+++ src/auth.js
@@ -19,24 +19,54 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
 const accessTokenSecret = 'youraccesstokensecret';
+const refreshTokenSecret = 'yourrefreshtokensecrethere';
+const refreshTokens = [];
 
 app.post('/login', (req, res) => {
-    // Read username and password from request body
+    // read username and password from request body
     const { username, password } = req.body;
 
-    // Filter user from the users array by username and password
+    // filter user from the users array by username and password
     const user = users.find(u => { return u.username === username && u.password === password });
 
     if (user) {
-        // Generate an access token
-        const accessToken = jwt.sign({ username: user.username,  role: user.role }, accessTokenSecret);
+        // generate an access token
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+        const refreshToken = jwt.sign({ username: user.username, role: user.role }, refreshTokenSecret);
+
+        refreshTokens.push(refreshToken);
 
         res.json({
-            accessToken
+            accessToken,
+            refreshToken
         });
     } else {
         res.send('Username or password incorrect');
     }
+});
+
+app.post('/token', (req, res) => {
+    const { token } = req.body;
+
+    if (!token) {
+        return res.sendStatus(401);
+    }
+
+    if (!refreshTokens.includes(token)) {
+        return res.sendStatus(403);
+    }
+
+    jwt.verify(token, refreshTokenSecret, (err, user) => {
+        if (err) {
+            return res.sendStatus(403);
+        }
+
+        const accessToken = jwt.sign({ username: user.username, role: user.role }, accessTokenSecret, { expiresIn: '20m' });
+
+        res.json({
+            accessToken
+        });
+    });
 });
 
 app.listen(3000, () => {
